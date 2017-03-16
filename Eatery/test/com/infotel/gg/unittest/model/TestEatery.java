8aa9658535050f19ff04cf8db6f0c8f99a4015ea
tpilon <tpilon05@gmail.com>
2017-03-16 12:49:29 +0100

    test package eatery constructeur Eatery

--- Eatery/test/com/infotel/gg/unittest/model/TestEatery.java
+++ Eatery/test/com/infotel/gg/unittest/model/TestEatery.java
@@ -7,9 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.infotel.gg.eatery.CookingStyle;
+import com.infotel.gg.eatery.Eatery;
 
 public class TestEatery {
 	CookingStyle c;
+	Eatery e;
 	
 	
 	@Before
@@ -21,21 +23,29 @@ public class TestEatery {
 		c = null;
 	}
 
+	// Tests constructeurs CookingStyle
 	@Test
-	public void constructeurOK() {
+	public void constructeurCookingOK() {
 		c = new CookingStyle(1, "Thomas");
 		assertNotNull(c);
 	}
 	
 	@Test
-	public void constructeurKO1() {
+	public void constructeurCookingKO1() {
 		c = new CookingStyle(null, "Thomas");
 		assertNull(c);
 	}
 	
 	@Test
-	public void constructeurKO2() {
+	public void constructeurCookingKO2() {
 		c = new CookingStyle(2, null);
 		assertNull(c);
+	}
+	
+	// Tests constructeurs Eatery
+	@Test
+	public void constructeurEateryOK() {
+//		e = new Eatery(id, name, description, executiveChef, practicalIformation, cookingStyle, address, menu, eateryTags, eateryManager);
+		assertNotNull(e);
 	}
 }
