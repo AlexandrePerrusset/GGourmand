52f62866aafce03f6bb1a9a173745c2c3bf4a4e2
AlexandrePerrusset <alex.perru@gmail.com>
2017-03-17 10:59:20 +0100

    Exception update

--- Eatery/src/com/infotel/gg/exception/GGourmandException.java
+++ Eatery/src/com/infotel/gg/exception/GGourmandException.java
@@ -1,31 +1,30 @@
 package com.infotel.gg.exception;
 
-public class AuthenticationException extends GGourmandException {
+public class GGourmandException extends Exception{
 
-	public AuthenticationException() {
+	public GGourmandException() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public AuthenticationException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+	public GGourmandException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
 		// TODO Auto-generated constructor stub
 	}
 
-	public AuthenticationException(String arg0, Throwable arg1) {
+	public GGourmandException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
 	}
 
-	public AuthenticationException(String arg0) {
+	public GGourmandException(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 
-	public AuthenticationException(Throwable arg0) {
+	public GGourmandException(Throwable arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 	
-
 }
