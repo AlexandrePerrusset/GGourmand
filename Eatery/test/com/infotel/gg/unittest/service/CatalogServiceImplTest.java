38df59ff16dee81854ba11d1d78232cd2cc25ee1
Rapha35 <raphaelle.tigeot@gmail.com>
2017-04-04 16:24:44 +0200

    pom xml

--- Eatery/test/com/infotel/gg/unittest/service/CatalogServiceImplTest.java
+++ Eatery/test/com/infotel/gg/unittest/service/CatalogServiceImplTest.java
@@ -10,7 +10,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -20,12 +19,13 @@ import com.infotel.gg.exception.GGourmandException;
 import com.infotel.gg.model.Eatery;
 import com.infotel.gg.model.ImageData;
 import com.infotel.gg.service.CatalogService;
-import com.infotel.gg.service.CatalogServiceImpl;
+
 
 import DBUnit.DBUtils;
 
 public class CatalogServiceImplTest {
 	List<CookingStyleDTO> cookstyledtos;
+	
 	static CatalogService catservice;
 	
 	Eatery eatery;
