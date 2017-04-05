6324e99d76fcb19f2c7c49859d7a7e32ed606e82
Rapha35 <raphaelle.tigeot@gmail.com>
2017-04-05 14:27:44 +0200

    image dto

---
+++ Eatery/src/com/infotel/gg/DTO/ImageDataDTO.java
@@ -0,0 +1,26 @@
+
+package com.infotel.gg.DTO;
+
+public class ImageDataDTO {
+	
+	private String content;
+	private String size;
+	
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+}
