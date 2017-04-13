4ec93dbba20d6aac081e1cd4d9c38105bcda540c
tpilon <tpilon05@gmail.com>
2017-04-13 15:04:39 +0200

    autoCompletion.js

--- SpringMVC/WebContent/js/autoCompletion.js
+++ SpringMVC/WebContent/js/autoCompletion.js
@@ -1,13 +1,15 @@
 $('#recherche').autocomplete({
-    source : function(){ // la fonction anonyme permet de maintenir une requête AJAX directement dans le plugin
-	$.ajax({
-            url : 'http://localhost:9200/gourmand/city/_search', // on appelle le script JSON
-            dataType : 'json', // on spécifie bien que le type de données est en JSON
-            crossDomain : true,
-            success : function(donnee){ // on se prépare à renvoyer les données réceptionnées grâce à l'évènement de succès
-            	return donnee.hits.hits._source.name;
-            }
-        });
-    },
-    minLength : 3
-});
+		source : function(requete, response) { // les deux arguments représentent les données nécessaires au plugin
+			$.ajax({
+				url : 'http://localhost:9200/gourmand/city/_search', // on appelle le script JSON
+				dataType : 'json', // on spécifie bien que le type de données est en JSON
+				crossDomain : true,
+				minlength : 3,
+				success : function(data) {
+					response($.map(data.hits.hits, function(item){
+					    return item._source.name;
+					}));
+				}
+			});
+		}
+	});
