eab4a17354ff0569e7e327996ca2103e2ffb7050
AlexandrePerrusset <alex.perru@gmail.com>
2017-04-25 15:19:11 +0200

    booking Rest

--- SpringRS/src/com/infotel/gg/rs/BookingController.java
+++ SpringRS/src/com/infotel/gg/rs/BookingController.java
@@ -1,9 +1,13 @@
 package com.infotel.gg.rs;
 
+import java.net.URLDecoder;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.infotel.gg.DTO.BookingDTO;
 import com.infotel.gg.DTO.CustomerDTO;
+import com.infotel.gg.DTO.UserDTO;
 import com.infotel.gg.exception.GGourmandException;
 import com.infotel.gg.model.Eatery;
 import com.infotel.gg.service.BookingService;
@@ -37,5 +42,20 @@ public class BookingController {
 		log.warn("reservation: " + dto);
 		
 		service.saveBookingRest(dto);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@RequestMapping(value="/bookings/{id}", method=RequestMethod.GET)
+	public List<BookingDTO> bookingByCustomer(@PathVariable("id") String id) throws GGourmandException{
+		
+		log.warn("reservation: " + id);
+		return service.findBookingsByCustomer(URLDecoder.decode(id));
+	}
+	
+	@RequestMapping(value="/bookings/customer", method=RequestMethod.POST)
+	public List<BookingDTO> bookingByCustomer(@RequestBody UserDTO uDto) throws GGourmandException{
+		
+		log.warn("reservation pour username : " + uDto.getUsername());
+		return service.findBookingsByCustomer(uDto.getUsername());
 	}
 }
